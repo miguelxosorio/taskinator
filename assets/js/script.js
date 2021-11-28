a5aef0e11e6da201a60ad2d76453ca94bbe5e306
vayne <miguel.osorio19@yahoo.com>
2021-11-27 22:30:39 -0600

    feature/form-submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,15 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");  // Create a new task item
     listItemEl.className = "task-item";             // Style the new task item
     listItemEl.textContent = "This is a new task."; // Add text
     tasksToDoEl.appendChild(listItemEl);            // Appends this element to the task list (bottom)
-    
-  };
+    console.log(event);
+};
 
-buttonEl.addEventListener("click", createTaskHandler);
+formEl.addEventListener("submit", createTaskHandler);
