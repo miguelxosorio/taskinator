31277ec9e67dbe1a369c9adc1041fdae69d28085
vayne <miguel.osorio19@yahoo.com>
2021-11-29 17:31:32 -0600

    troubleshooting feature/form-submit

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,13 @@ var taskFormHandler = function(event) {
   var taskNameInput = document.querySelector("input[name='task-name']").value;
   var taskTypeInput = document.querySelector("select[name='task-type']").value;
 
+  if (!taskNameInput || !taskTypeInput) {
+    alert("You need to fill out the task form!");
+    return false;
+
+    formEl.reset();
+  }
+
   // package up data as an object
   var taskDataObj = {
     name: taskNameInput,
@@ -17,9 +24,11 @@ var taskFormHandler = function(event) {
 }
 
 var createTaskEl = function(taskDataObj) {
+
   // create list item
 var listItemEl = document.createElement("li");
 listItemEl.className = "task-item";
+
 
 // create div to hold task info and add to list item
 var taskInfoEl = document.createElement("div");
@@ -28,8 +37,9 @@ taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span
 
 listItemEl.appendChild(taskInfoEl);
 
+
 // add entire list item to list
 tasksToDoEl.appendChild(listItemEl);
-}
+};
 
 formEl.addEventListener("submit", taskFormHandler);
